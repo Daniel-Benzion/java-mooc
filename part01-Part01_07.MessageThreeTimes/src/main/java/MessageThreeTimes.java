a54ecdcf650a49a8d0f3bee2ec0c25dd127289f9
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-05 15:34:48 -0500

    Message Three Times

---
+++ part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        
+        String input = scanner.nextLine();
+        
+        System.out.println(input);
+        System.out.println(input);
+        System.out.println(input);
+
+    }
+}
