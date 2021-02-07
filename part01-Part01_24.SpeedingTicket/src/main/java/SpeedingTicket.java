c43796202f884bac96a675e7f371de159bf30bee
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-07 10:37:07 -0500

    Speeding Ticket

---
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SpeedingTicket {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here. 
+        System.out.println("Give speed:");
+        int input = Integer.valueOf(scanner.nextLine());
+        
+        if (input > 120) System.out.println("Speeding ticket!");
+    }
+}
