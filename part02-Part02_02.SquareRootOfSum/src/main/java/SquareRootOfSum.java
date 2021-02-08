e3c1ce25cd3c528eca539ea81826dbf534cb8b99
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-08 11:11:53 -0500

    Square Root of Sum

---
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.valueOf(scanner.nextLine());
+        int num2 = Integer.valueOf(scanner.nextLine());
+        System.out.println(Math.sqrt(num1 + num2));
+    }
+}
