39c0275a04f9a88099cbacc3a9c85ba68e38b5be
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-12 14:49:20 -0500

    From Parameter to One

---
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -0,0 +1,17 @@
+
+
+public class FromParameterToOne {
+
+    public static void main(String[] args) {
+
+
+    }
+    
+    public static void printFromNumberToOne(int number) {
+        while (number > 0) {
+            System.out.println(number);
+            number--;
+        }
+    }
+
+}
