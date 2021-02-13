dc6815a31ef9e67096366c13d34a7273cdc5e1fd
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-13 08:57:55 -0500

    Index of Smallest

---
+++ part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOfSmallest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // implement here a program that reads user input
+        // until the user enters 9999
+        
+        // after that, the program prints the smallest number
+        // and its index -- the smallest number
+        // might appear multiple times
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 9999) {
+                break;
+            }
+            list.add(input);
+        }
+        int smallest = list.get(0);
+        for (int number : list) {
+            if (number < smallest) {
+                smallest = number;
+            }
+        }
+        System.out.println("Smallest number: " + smallest);
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if (number == smallest) {
+                System.out.println("Found at index: " + i);
+            }
+        }
+    }
+}
