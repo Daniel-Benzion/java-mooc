ecdc49bda0dc918af75bc488f4ee70c0391b9c87
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-14 11:47:25 -0500

    Print Neatly

---
+++ part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -0,0 +1,21 @@
+
+public class ArrayPrinter {
+
+    public static void main(String[] args) {
+        // You can test your method here
+        int[] array = {5, 1, 3, 4, 2};
+        printNeatly(array);
+    }
+
+    public static void printNeatly(int[] array) {
+        // Write some code in here
+        StringBuilder stringbuilder = new StringBuilder();
+        for (int i = 0; i < array.length - 1; i++) {
+            stringbuilder.append(array[i]);
+            stringbuilder.append(", ");
+        }
+        stringbuilder.append(array[array.length - 1]);
+        String joined = stringbuilder.toString();
+        System.out.println(joined);
+    }
+}
