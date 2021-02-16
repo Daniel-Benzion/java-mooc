0cd1ebad96a5e40f691697dbc63fdafa5fa09ae3
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-16 13:15:20 -0500

    Your First Account

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account account = new Account("Dan's account", 100.0);
+        account.deposit(20.0);
+        System.out.println(account);
+    }
+}
