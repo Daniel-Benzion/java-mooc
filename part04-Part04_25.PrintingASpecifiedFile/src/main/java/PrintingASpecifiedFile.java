da5dcf2bff402702214047e99db5235f5a00a8f8
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-19 17:14:56 -0500

    Printing a Specified File

---
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -0,0 +1,19 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class PrintingASpecifiedFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String file = scanner.nextLine();
+        try (Scanner getFile = new Scanner(Paths.get(file))) {
+            while (getFile.hasNextLine()) {
+                String row = getFile.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
