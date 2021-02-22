6460f262a341ec98ffdc43ddced540d8461edaa9
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-21 19:28:24 -0500

    FitByte

---
+++ part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ionbenz
+ */
+public class Fitbyte {
+    
+    private int age;
+    private int restingHeartRate;
+    
+    
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+    }
+    
+     public double targetHeartRate(double percentageOfMaximum) {
+         
+        double max = 206.3 - (0.711 * age);
+        return (max - restingHeartRate) * (percentageOfMaximum) + restingHeartRate;
+     }
+}
