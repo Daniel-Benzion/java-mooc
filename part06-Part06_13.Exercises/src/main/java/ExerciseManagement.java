2103ab68bc17e8400100cd88cb204af372f43919
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-08 17:09:05 -0500

    Exercises

---
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+
+public class ExerciseManagement {
+    
+    private ArrayList<Exercise> exercises;
+    
+    
+    public ExerciseManagement(){
+        this.exercises = new ArrayList<>();
+    }
+    
+    public ArrayList<String> exerciseList(){
+        ArrayList <String> list = new ArrayList<>();
+        for(Exercise exercise : exercises){
+            list.add(exercise.getName());
+        }
+        return list;
+    }
+    
+    public void add(String exercise){
+        this.exercises.add(new Exercise(exercise));
+    }
+    
+    public void markAsCompleted(String exercise){
+        for(Exercise e : exercises){
+            if (e.getName().equals(exercise)){
+                e.setCompleted(true);
+            }
+        }
+    }
+    
+    public boolean isCompleted(String exercise){
+        for (Exercise e : exercises){
+            if(e.getName().equals(exercise)){
+                return e.isCompleted();
+            }
+        }
+        return false;
+    }
+    
+}
