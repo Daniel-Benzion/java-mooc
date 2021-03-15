84ad8fe63b5589ab8d8a0f83d5b2e8c1b815c4e6
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-15 17:09:04 -0400

    Recipe Search

---
+++ part07-Part07_07.RecipeSearch/src/main/java/RecipeSearch.java
@@ -0,0 +1,13 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RecipeSearch {
+
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.start();
+    }
+
+}
